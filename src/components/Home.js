da136d4f398e135ee88f68cd283ffebd6e82864c
vsrcreativefactory <141257315+vsrcreativefactory@users.noreply.github.com>
2024-12-06 22:06:05 +0530

    some changes made

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,3 @@ import React from "react";
 import Child from "./Child";
 import "./Home.css";
 
-const Home = () => {
-    return(
-    <div>
-        <h1 className="h1">Home component</h1>
-        <Child />
-    </div>
-    )
-}
-
-export default Home;
